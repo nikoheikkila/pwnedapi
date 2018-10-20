bc106d25af60c915fece7ee1317ac10b9beceb3f
rachmadaniHaryono <foreturiga@gmail.com>
2018-10-21 06:55:56 +0800

    chg: dev: main cli module
    - main cli func
    - 2 subcommand

---
+++ pwnedapi/__main__.py
@@ -0,0 +1,27 @@
+from . import Password, Scanner
+
+import click
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.password_option(help="Password, which will be checked.")
+def check(password):
+    "Checks a single password if it has been pwned."
+    print('password:{}'.format(password))
+    password = Password(password)
+    if password.is_pwned():
+        print(f"Your password has been pwned {password.pwned_count} times.")
+
+
+@cli.command()
+@click.argument('INPUT_FILE', type=click.File('rb'))
+@click.option('--output-format', help='Output data format.', default='csv')
+def scan(input_file, output_format='csv'):
+    "Scan a file for pwned passwords."
+    scanner = Scanner()
+    scanner.scan(input_file.name)
+    print(scanner.export(output_format))
