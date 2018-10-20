515d3ae5a4f9755e6b29fe0e4e35d1b4006e819c
rachmadaniHaryono <foreturiga@gmail.com>
2018-10-21 07:34:21 +0800

    new: test: main module test

---
+++ pwnedapi/tests/main_test.py
@@ -0,0 +1,40 @@
+from tempfile import NamedTemporaryFile
+import re
+
+from click.testing import CliRunner
+import click
+import pytest
+
+from pwnedapi import __main__ as main
+
+PASSWORDS = [
+    "dog",
+    "cat",
+    "cuckoo"
+]
+
+
+@pytest.fixture()
+def tempfile():
+    tmpfile = NamedTemporaryFile(delete=False)
+    tmpfile.write(str.encode("\n".join(PASSWORDS)))
+    return tmpfile.name
+
+
+def test_check():
+    runner = CliRunner()
+    result = runner.invoke(main.check, input='Peter\nPeter')
+    assert result.exit_code == 0
+    patt = \
+        r'Password: \nRepeat for confirmation: \npassword:Peter\n' \
+        r'Your password has been pwned \d+ times.\n'
+    assert re.match(patt, result.output)
+
+
+def test_scan(tempfile):
+    runner = CliRunner()
+    result = runner.invoke(main.scan, [tempfile])
+    assert result.exit_code == 0
+    #  assert result.output == 'Hello Peter!\n'
+    patt = r'Password,Leak Count\ndog,\d+\ncat,\d+\ncuckoo,\d+\n\n'
+    assert re.match(patt, result.output)
